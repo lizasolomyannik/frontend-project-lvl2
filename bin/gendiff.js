f140bfbcf41acad6c3ddda8300d32fa3d9dfc87e
elizaveta.s <easolomyannik@icloud.com>
2022-07-13 20:05:21 +0300

    fix commander.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -11,8 +11,9 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .option('-f, --format <type>', 'output format')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
-  .action(getDiff);
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(getDiff(filepath1, filepath2));
+  });
 
 program.parse();
