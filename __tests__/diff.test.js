ff1b818bc8e55a5c6efa2e87c6d7991c4e461c8f
elizaveta.s <easolomyannik@icloud.com>
2022-08-02 20:05:31 +0300

    remove duplicates

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -7,34 +7,31 @@ import parseFile from '../src/parsers.js';
 const getFixturePath = (filename) => path.join('__fixtures__', filename);
 const readTextFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
+const JSONpath1 = 'file1.json';
+const JSONpath2 = 'file2.json';
+const YAMLpath1 = 'file1.yaml';
+const YAMLpath2 = 'file2.yaml';
+
 const stylishExpectedResult = readTextFile('stylishExpectedResult');
 const plainExpectedResult = readTextFile('plainExpectedResult').trim();
 
 test('stylish compare JSON files', () => {
-  const path1 = 'file1.json';
-  const path2 = 'file2.json';
-  const stylishResult = genDiff(path1, path2, 'stylish');
+  const stylishResult = genDiff(JSONpath1, JSONpath2, 'stylish');
   expect(stylishResult).toBe(stylishExpectedResult);
 });
 
 test('stylish compare YAML files', () => {
-  const path1 = 'file1.yaml';
-  const path2 = 'file2.yaml';
-  const stylishResult = genDiff(path1, path2, 'stylish');
+  const stylishResult = genDiff(YAMLpath1, YAMLpath2, 'stylish');
   expect(stylishResult).toBe(stylishExpectedResult);
 });
 
 test('plain compare JSON files', () => {
-  const path1 = 'file1.json';
-  const path2 = 'file2.json';
-  const plainResult = genDiff(path1, path2, 'plain');
+  const plainResult = genDiff(JSONpath1, JSONpath2, 'plain');
   expect(plainResult).toBe(plainExpectedResult);
 });
 
 test('plain compare YAML files', () => {
-  const path1 = 'file1.yaml';
-  const path2 = 'file2.yaml';
-  const plainResult = genDiff(path1, path2, 'plain');
+  const plainResult = genDiff(YAMLpath1, YAMLpath2, 'plain');
   expect(plainResult).toBe(plainExpectedResult);
 });
 
