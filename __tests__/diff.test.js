09595aa9561df72cb484946671a9a6e1d1421e16
elizaveta.s <easolomyannik@icloud.com>
2022-08-02 19:58:14 +0300

    fix formatter errors

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -1,10 +1,8 @@
 import path from 'path';
 import * as fs from 'fs';
 import { expect, test } from '@jest/globals';
-import getDiff from '../src/index.js';
+import genDiff from '../src/formatters/index.js';
 import parseFile from '../src/parsers.js';
-import stylish from '../src/formatters/stylish.js';
-import plain from '../src/formatters/plain.js';
 
 const getFixturePath = (filename) => path.join('__fixtures__', filename);
 const readTextFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
@@ -13,30 +11,30 @@ const stylishExpectedResult = readTextFile('stylishExpectedResult');
 const plainExpectedResult = readTextFile('plainExpectedResult').trim();
 
 test('stylish compare JSON files', () => {
-  const path1 = getFixturePath('file1.json');
-  const path2 = getFixturePath('file2.json');
-  const stylishResult = stylish(getDiff(path1, path2));
+  const path1 = 'file1.json';
+  const path2 = 'file2.json';
+  const stylishResult = genDiff(path1, path2, 'stylish');
   expect(stylishResult).toBe(stylishExpectedResult);
 });
 
 test('stylish compare YAML files', () => {
-  const path1 = '__fixtures__/file1.yaml';
-  const path2 = '__fixtures__/file2.yaml';
-  const result = stylish(getDiff(path1, path2));
-  expect(result).toBe(stylishExpectedResult);
+  const path1 = 'file1.yaml';
+  const path2 = 'file2.yaml';
+  const stylishResult = genDiff(path1, path2, 'stylish');
+  expect(stylishResult).toBe(stylishExpectedResult);
 });
 
 test('plain compare JSON files', () => {
-  const path1 = getFixturePath('file1.json');
-  const path2 = getFixturePath('file2.json');
-  const plainResult = plain(getDiff(path1, path2));
+  const path1 = 'file1.json';
+  const path2 = 'file2.json';
+  const plainResult = genDiff(path1, path2, 'plain');
   expect(plainResult).toBe(plainExpectedResult);
 });
 
 test('plain compare YAML files', () => {
-  const path1 = '__fixtures__/file1.yaml';
-  const path2 = '__fixtures__/file2.yaml';
-  const plainResult = plain(getDiff(path1, path2));
+  const path1 = 'file1.yaml';
+  const path2 = 'file2.yaml';
+  const plainResult = genDiff(path1, path2, 'plain');
   expect(plainResult).toBe(plainExpectedResult);
 });
 
